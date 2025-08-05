5e7f283ffb29845b8c0dbe86083440643814bdc4
elisha <elishajameel2007@gmail.com>
2025-08-05 12:13:57 +0500

    increased the speed of cube

--- src/components/ThreeScene.jsx
+++ src/components/ThreeScene.jsx
@@ -57,8 +57,8 @@ export default function ThreeScene() {
     const animate = () => {
       requestAnimationFrame(animate);
 
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
+      cube.rotation.x += 0.02;
+      cube.rotation.y += 0.02;
       renderer.render(scene, camera);
     };
     // start the dance
